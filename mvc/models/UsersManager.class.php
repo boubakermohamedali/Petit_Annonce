27bd57352ab89eb5a4e4d06d80f3ca826311c1d1
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-07-26 12:52:03 +0200

    git

--- mvc/models/UsersManager.class.php
+++ mvc/models/UsersManager.class.php
@@ -33,20 +33,20 @@ class UsersManager extends Model
         /** vous pouvez écrire les requêtes pour les différents managers de DB, ou bien vous focaliser sur celui de votre choix */
         if (DB_MANAGER == PDO) // version PDO
         {
-            $req = $this->getDatabase()->prepare("SELECT * FROM users ");
+            $req = $this->getDatabase()->prepare("SELECT * FROM utilisateurs ");
             $req->execute();
             $users = $req->fetchAll(PDO::FETCH_ASSOC);
             $req->closeCursor();
         }
         else if (DB_MANAGER == MEDOO) // version MEDOO
         {
-            $users = $this->getDatabase()->select("users", "*") ;
+            $users = $this->getDatabase()->select("utilisateurs", "*") ;
         }
 
         // on a récupéré tous les utilisateurs, on les ajoute au manager de users
         foreach ($users as $user) {
             $new_user = new User(
-                $user['id'],
+                $user['id_utilisateur'],
                 $user['username'],
                 $user['password']
             );
