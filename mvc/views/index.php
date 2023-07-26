7127cbc4d4db39235651bf56c791eb2cf4bd663b
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-07-26 11:09:21 +0200

    commit

--- mvc/views/index.php
+++ mvc/views/index.php
@@ -1,222 +1,133 @@
-<!DOCTYPE HTML>
-<!--
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Hyperspace by HTML5 UP</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="public/assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="public/assets/css/noscript.css" /></noscript>
-	</head>
-	<body class="is-preload">
+<?php include 'header.php'; ?>
+<br/>
+    <div class='row'>
+        <div class='jumbotron bg-light m-2 p-2'>
+            <h1 class='display-4'>Bienvenue au petit annonce!</h1>
+            <header>
+              <a href="index.php"><img src="images/pinterest.png" alt="petit_annonce!"></a>
+            </header>
+            <br>
+            <p class='lead'>Ici vous pouvez gérer les abonnement pour le très exclusif petit annonce !</p>
+            <hr class='my-4'>
+            <p>Cliquer sur un des boutons ci-dessous pour obtenir une liste des membres ou des types categorie</p>
+            <br><br>
+            <p class='lead'>
+            <a href='index.php' class='btn btn-secondary m-2 active' role='button'>Accueil</a>
+            <a href='annonces.php' class='btn btn-secondary m-2 active' role='button'>Annonces</a>
+            <a href='categorie.php' class='btn btn-secondary m-2 active' role='button'>Categories</a>
+            <a href='members.php' class='btn btn-secondary m-2 active' role='button'>Membres</a>
+            <a href='login.php' class='btn btn-secondary m-2 active' role='button'>Contacts</a>
+            </p>
+        </div>
+    </div>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="base.css" />
+        <link rel="stylesheet" href="book.css" />
+        <link rel="stylesheet" href="login.css" />
+        <link rel="stylesheet" href="index.css" />
+        <link href="style.css" rel="stylesheet">
+        <link id="codyframe" rel="stylesheet" href="style.css">
+        <title>petit_annonce! - Contact</title>
+        <script>document.getElementsByTagName("html")[0].className += " js";</script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <title>Connexion | petit annonce</title>
+<br><br/>
+<br/>
+<div class="container">
+	<div class="m-4">
+		<div class="from-group">
+			<from action="pdf.php" methode="POST">
+				<imput type="Test" name="petit_annonce" aria-placeholder="votre petit_annonce" class="form-control"/><br/>
+				<button type="submit" class="btn btn-primary">PDF</button>
+			</from>
+		</div>
+	</div>
+</div>
+<br><br/>
+<?php
 
-    <!-- Header -->
-    <header id="header">
-        <a href="index" class="title">Hyperspace</a>
-        <nav>
-            <ul>
-                <li><a href="index" class="<?= str_contains(FULL_URL, "index") || str_contains(FULL_URL, "home") ? "active" : "" ?>">Home</a></li>
-                <li><a href="generic" class="<?= str_contains(FULL_URL, "generic") ? "active" : "" ?>">Generic</a></li>
-                <li><a href="elements"  class="<?= str_contains(FULL_URL, "elements") ? "active" : "" ?>">Elements</a></li>
-                <li><a href="allusers"  class="<?= str_contains(FULL_URL, "allusers") ? "active" : "" ?>">Utilisateurs</a></li>
-            </ul>
-        </nav>
-    </header>
+require_once 'connexion.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $test = LoginUser();
+    if ($test[0] === "success") {
+        header("Location: index.php");
+    }
+    print_r($test);
+}
+?>
+<?php require_once 'header.php' ?>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <h2>Connexion</h2>
+        <?php if (isset($error)): ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $error; ?>
+            </div>
+        <?php endif;
+        $sessionStatus = session_status();
 
-		<!-- Sidebar -->
-			<section id="sidebar">
-				<div class="inner">
-					<nav>
-						<ul>
-							<li><a href="#intro">Welcome</a></li>
-							<li><a href="#one">Who we are</a></li>
-							<li><a href="#two">What we do</a></li>
-							<li><a href="#three">Get in touch</a></li>
-						</ul>
-					</nav>
-				</div>
-			</section>
+        if ($sessionStatus === PHP_SESSION_DISABLED) {
+            echo "Les sessions sont désactivées sur le serveur.";
+        } elseif ($sessionStatus === PHP_SESSION_NONE) {
+            echo "Aucune session n'est active.";
+        } elseif ($sessionStatus === PHP_SESSION_ACTIVE) {
+            echo "Une session est active.";
+        } ?>
+        <form method="post" action="">
+            <div class="form-group">
+                <label for="email">Mail membre</label>
+                <input type="text" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="hash_">Mot de passe</label>
+                <input type="text" class="form-control" id="hash_" name="hash_" required>
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Se connecter</button>
+            <a href="mdp-oubli.php">Mot de passe oublié ?</a>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+    
+        </form>
+    </div>
+</div>
 
-				<!-- Intro -->
-					<section id="intro" class="wrapper style1 fullscreen fade-up">
-						<div class="inner">
-							<h1>Hyperspace</h1>
-							<p>Just another fine responsive site template designed by <a href="http://html5up.net">HTML5 UP</a><br />
-							and released for free under the <a href="http://html5up.net/license">Creative Commons</a>.</p>
-							<ul class="actions">
-								<li><a href="#one" class="button scrolly">Learn more</a></li>
-							</ul>
-						</div>
-					</section>
-
-				<!-- One -->
-					<section id="one" class="wrapper style2 spotlights">
-						<section>
-							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Sed ipsum dolor</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="generic.html" class="button">Learn more</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section>
-							<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Feugiat consequat</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="generic.html" class="button">Learn more</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-						<section>
-							<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
-							<div class="content">
-								<div class="inner">
-									<h2>Ultricies aliquam</h2>
-									<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-									<ul class="actions">
-										<li><a href="generic.html" class="button">Learn more</a></li>
-									</ul>
-								</div>
-							</div>
-						</section>
-					</section>
-
-				<!-- Two -->
-					<section id="two" class="wrapper style3 fade-up">
-						<div class="inner">
-							<h2>What we do</h2>
-							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
-							<div class="features">
-								<section>
-									<span class="icon solid major fa-code"></span>
-									<h3>Lorem ipsum amet</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon solid major fa-lock"></span>
-									<h3>Aliquam sed nullam</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon solid major fa-cog"></span>
-									<h3>Sed erat ullam corper</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon solid major fa-desktop"></span>
-									<h3>Veroeros quis lorem</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon solid major fa-link"></span>
-									<h3>Urna quis bibendum</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon major fa-gem"></span>
-									<h3>Aliquam urna dapibus</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-							</div>
-							<ul class="actions">
-								<li><a href="generic.html" class="button">Learn more</a></li>
-							</ul>
-						</div>
-					</section>
-
-				<!-- Three -->
-					<section id="three" class="wrapper style1 fade-up">
-						<div class="inner">
-							<h2>Get in touch</h2>
-							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
-							<div class="split style1">
-								<section>
-									<form method="post" action="#">
-										<div class="fields">
-											<div class="field half">
-												<label for="name">Name</label>
-												<input type="text" name="name" id="name" />
-											</div>
-											<div class="field half">
-												<label for="email">Email</label>
-												<input type="text" name="email" id="email" />
-											</div>
-											<div class="field">
-												<label for="message">Message</label>
-												<textarea name="message" id="message" rows="5"></textarea>
-											</div>
-										</div>
-										<ul class="actions">
-											<li><a href="" class="button submit">Send Message</a></li>
-										</ul>
-									</form>
-								</section>
-								<section>
-									<ul class="contact">
-										<li>
-											<h3>Adresse</h3>
-											<span>17 Boulevard Victor Hugo <br />
-											06000 Nice<br />
-											FRANCE</span>
-										</li>
-										<li>
-											<h3>Email</h3>
-											<a href="#">user@untitled.tld</a>
-										</li>
-										<li>
-											<h3>Phone</h3>
-											<span>04 93 87 72 44</span>
-										</li>
-										<li>
-											<h3>Social</h3>
-											<ul class="icons">
-												<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-												<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-												<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-												<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-												<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-											</ul>
-										</li>
-									</ul>
-								</section>
-							</div>
-						</div>
-					</section>
-
-			</div>
-
-		<!-- Footer -->
-			<footer id="footer" class="wrapper style1-alt">
-				<div class="inner">
-					<ul class="menu">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-					</ul>
-				</div>
-			</footer>
-
-		<!-- Scripts -->
-			<script src="public/assets/js/jquery.min.js"></script>
-			<script src="public/assets/js/jquery.scrollex.min.js"></script>
-			<script src="public/assets/js/jquery.scrolly.min.js"></script>
-			<script src="public/assets/js/browser.min.js"></script>
-			<script src="public/assets/js/breakpoints.min.js"></script>
-			<script src="public/assets/js/util.js"></script>
-			<script src="public/assets/js/main.js"></script>
-
-	</body>
+<?php require_once 'footer.php'; ?>
+<br/><br/><br/>
+        <!-- /**** Footer ****/ -->
+<footer>
+  <div class="partenaires">
+    <h5>Site partenaires</h5>
+    <a href="https://www.amazon.fr">Amazon</a>
+    <a href="https://www.fnac.com/">Fnac</a>
+    <a href="https://www.ebay.fr/">Ebay</a>
+    <a href="https://www.waterstones.com">Waterstones</a>
+  </div>
+  <div class="center">
+    <div class="coordonner">
+      <img src="images/logo.png" alt="">
+      <a href="">22 Avenue lamartine</a>
+      <a href="">06000 Nice</a>
+      <a href="">04 92 63 53 43</a>
+    </div>
+    <div class="reseau">
+      <a href="https://www.facebook.com/"><img src="images/facebook.png" alt=""></a>
+        <a href="https://www.twitter.com"><img src="images/twitter.png" alt=""></a>
+        <a href="https://www.instagram.com/"><img src="images/insta.png" alt=""></a>
+    </div>
+  </div>
+  <div class="dons">
+    <h5>Faites un don</h5>
+    <button class="btn"> <a href="PDF/COFFEE_SHOP.pdf" download="Annonces">Télécharger le produit en PDF</a></button>
+  </div>
+</footer>
+<!-- /**** Footer fin ****/ -->
+        <script src="scripts.js"></script>
+    </body>
 </html>
+<?php require_once 'footer.php' ?>
