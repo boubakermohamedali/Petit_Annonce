b52190cc0ab0b3712bed3923e8a189832ec033ff
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-07-26 10:47:52 +0200

    commit

--- mvc/views/index.php
+++ mvc/views/index.php
@@ -22,7 +22,7 @@
                 <li><a href="index" class="<?= str_contains(FULL_URL, "index") || str_contains(FULL_URL, "home") ? "active" : "" ?>">Home</a></li>
                 <li><a href="generic" class="<?= str_contains(FULL_URL, "generic") ? "active" : "" ?>">Generic</a></li>
                 <li><a href="elements"  class="<?= str_contains(FULL_URL, "elements") ? "active" : "" ?>">Elements</a></li>
-                <li><a href="allusers"  class="<?= str_contains(FULL_URL, "allusers") ? "active" : "" ?>">Users</a></li>
+                <li><a href="allusers"  class="<?= str_contains(FULL_URL, "allusers") ? "active" : "" ?>">Utilisateurs</a></li>
             </ul>
         </nav>
     </header>
